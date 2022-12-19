229b626ceca74af9c446f8de26a0c5698694d33b
Roqi SB <roqisb9@gmail.com>
2022-12-19 16:11:00 +0700

    Add project files.

---
+++ EXE5_20210140114_KLMPOK2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20210140114_KLMPOK2
+{
+    class node
+    {
+        int FRONT, REAR;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
