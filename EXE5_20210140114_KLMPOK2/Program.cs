18b06df0490ac8631825f19c44fe234a4d8b4815
Roqi SB <roqisb9@gmail.com>
2022-12-19 16:59:19 +0700

    create method display

--- EXE5_20210140114_KLMPOK2/Program.cs
+++ EXE5_20210140114_KLMPOK2/Program.cs
@@ -58,5 +58,41 @@ namespace EXE5_20210140114_KLMPOK2
                     ROQI = ROQI + 1;
             }
         }
+        public void display()
+        {
+            int ROQI_position = ROQI;
+            int BACHRI_position = BACHRI;
+            if (ROQI == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are ........................\n");
+            if (ROQI_position <= BACHRI_position)
+            {
+                while (ROQI_position <= BACHRI_position)
+                {
+                    Console.Write(queue_array[ROQI_position] + "    ");
+                    ROQI_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                while (ROQI_position <= max - 1)
+                {
+                    Console.Write(queue_array[ROQI_position] + "    ");
+                    ROQI_position++;
+                }
+                ROQI_position = 0;
+                while (ROQI_position <= BACHRI_position)
+                {
+                    Console.Write(queue_array[ROQI_position] + "     ");
+                    ROQI_position++;
+                }
+                Console.WriteLine();
+            }
+        }
+     
     }
 }
