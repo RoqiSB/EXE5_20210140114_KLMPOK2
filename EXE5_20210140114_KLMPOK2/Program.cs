6d9b99d74d65436ad702e9fd8367d6afb358c5e1
Roqi SB <roqisb9@gmail.com>
2022-12-19 16:54:12 +0700

    create method insert

--- EXE5_20210140114_KLMPOK2/Program.cs
+++ EXE5_20210140114_KLMPOK2/Program.cs
@@ -6,15 +6,35 @@ using System.Threading.Tasks;
 
 namespace EXE5_20210140114_KLMPOK2
 {
-    class node
+    class Node
     {
-        int ROQI, BACHRI;
-    }
-    
-    internal class Program
-    {
-        static void Main(string[] args)
+        int ROQI, BACHRI, max = 5;
+        int[] queue_array = new int[5];
+        public Node()
         {
+            ROQI = -1;
+            BACHRI = -1;
+        }
+        public void insert(int element)
+        {
+            if ((ROQI == 0 && BACHRI == max - 1) || (ROQI == BACHRI + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            if (ROQI == -1)
+            {
+                ROQI = 0;
+                BACHRI = 0;
+            }
+            else
+            {
+                if (BACHRI == max - 1)
+                    BACHRI = 0;
+                else
+                    BACHRI = BACHRI + 1;
+            }
+            queue_array[BACHRI] = element;
         }
     }
 }
